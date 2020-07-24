f5dbaf0c31392fe005fdc541de16d6f0070a3326
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-23 18:56:27 -0600

    added data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-fetch("https://api.thecatapi.com/v1/images/search?breed_id=beng") //Sends the request to fetch data
+/*fetch("https://api.thecatapi.com/v1/images/search?breed_id=beng") //Sends the request to fetch data
 .then(response=> { 
     if (response.status>=200 && response.status<=299) {  // checks the range of response codes 
         return response.json(); // converts response to JSON and sends to .then
@@ -27,5 +27,44 @@ for (const cbreed of catsBreed) //looping through array
 //Add ul to the body of webpage
 document.body.appendChild(catsBreedUL);
 });
+*/
+
+axios.get("https://api.thecatapi.com/v1/images/search?breed_id=beng") //type of request id GET
+//Handle reponse...
+ .then(response=> {
+     //Test if we are getting data
+     console.log(response);
+     
+     const cbreed1= response.data.breeds; // Object contatining id and name properties.
+    /**
+     * <DL>
+     *  <DT>Title of item(s)</DT>
+     *  <DD>Item Contents</DD>
+     * <DT>Title of item(s)</DT>
+     *  <DD>Item Contents</DD>
+     * <DD>Item Contents</DD>
+     * </DL>
+     */
+    const cbreed1DL = document.createElement( "DL" );
+    const cbreed1DT = document.createElement( "DT" );
+    cbreed1DT.textContent = "ID";
+    const cBreed1DD = document.createElement( "DD" );
+    cBreed1DD.textContent = cbreed1.id; // Get the value from our id property (see response.)
+    const cNameDT = document.createElement( "DT" );
+    cNameDT.textContent = "Name";
+    const cNameDD = document.createElement( "DD" );
+    cNameDD.textContent = cbreed1.name; // Get the value from our name property (see response.)
+
+     //Append all the things to <DL>
+     cbreed1DL.appendChild( cbreed1DT );
+     cbreed1DL.appendChild( cBreed1DD );
+     cbreed1DL.appendChild( cNameDT );
+     cbreed1DL.appendChild( cNameDD );
+    // Put this into the page (<body>)!
+    document.body.appendChild( cbreed1DL );
+
+
+ });
+
 
 
