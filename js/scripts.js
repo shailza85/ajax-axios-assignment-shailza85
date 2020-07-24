0d7cd7c54860afafa6cb8757c25e7fc4a8fd859b
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-23 22:28:09 -0600

    added 2 ApI's data to test.

--- js/scripts.js
+++ js/scripts.js
@@ -29,13 +29,14 @@ document.body.appendChild(catsBreedUL);
 });
 */
 
-axios.get("https://api.thecatapi.com/v1/images/search?breed_id=beng") //type of request id GET
+axios.get("https://api.abalin.net/today?country=us&timezone=America%2FLos_Angeles") //type of request id GET
 //Handle reponse...
  .then(response=> {
      //Test if we are getting data
-     console.log(response);
+    // console.log(response);
      
-     const cbreed1= response.data.breeds; // Object contatining id and name properties.
+     const nDate= response.data.data; // Object contatining id and name properties.
+     //console.log(nDate);
     /**
      * <DL>
      *  <DT>Title of item(s)</DT>
@@ -45,15 +46,59 @@ axios.get("https://api.thecatapi.com/v1/images/search?breed_id=beng") //type of 
      * <DD>Item Contents</DD>
      * </DL>
      */
+    const namedAPI= document.createElement("h2");
+    namedAPI.textContent="International Nameday API:";
+    const nDateDL = document.createElement( "DL" );
+    const nDateDT = document.createElement( "DT" );
+    nDateDT.textContent = "Date Today:";
+    const nDateDD = document.createElement( "DD" );
+    nDateDD.textContent = `Day: ${nDate.dates.day} Month: ${nDate.dates.month}`; // Get the value from our id property (see response.)
+    const nDayDT = document.createElement( "DT" );
+    nDayDT.textContent = "Name Days of Country-US:";
+    const nDayDD = document.createElement( "DD" );
+    nDayDD.textContent = nDate.namedays.us; // Get the value from our name property (see response.)
+
+     //Append all the things to <DL>
+
+     nDateDL.appendChild( nDateDT );
+     nDateDL.appendChild( nDateDD );
+     nDateDL.appendChild( nDayDT );
+     nDateDL.appendChild( nDayDD );
+    // Put this into the page (<body>)!
+    document.body.appendChild( namedAPI );
+    document.body.appendChild( nDateDL );
+
+ });
+
+
+ axios.get("https://dog.ceo/api/breeds/list/all") //type of request id GET
+//Handle reponse...
+ .then(response=> {
+     //Test if we are getting data
+    // console.log(response);
+     
+     const cbreed1= response.data.message; // Object contatining id and name properties.
+     //console.log(cbreed1);
+    /**
+     * <DL>
+     *  <DT>Title of item(s)</DT>
+     *  <DD>Item Contents</DD>
+     * <DT>Title of item(s)</DT>
+     *  <DD>Item Contents</DD>
+     * <DD>Item Contents</DD>
+     * </DL>
+     */
+    const cHeading = document.createElement("h2");
+    cHeading.textContent ="Dog Breeds List";
     const cbreed1DL = document.createElement( "DL" );
     const cbreed1DT = document.createElement( "DT" );
-    cbreed1DT.textContent = "ID";
+    cbreed1DT.textContent = "Breed";
     const cBreed1DD = document.createElement( "DD" );
-    cBreed1DD.textContent = cbreed1.id; // Get the value from our id property (see response.)
+    cBreed1DD.textContent = cbreed1.australian; // Get the value from our id property (see response.)
     const cNameDT = document.createElement( "DT" );
-    cNameDT.textContent = "Name";
+    cNameDT.textContent = "Sub:Breed";
     const cNameDD = document.createElement( "DD" );
-    cNameDD.textContent = cbreed1.name; // Get the value from our name property (see response.)
+    cNameDD.textContent = cbreed1.bulldog; // Get the value from our name property (see response.)
 
      //Append all the things to <DL>
      cbreed1DL.appendChild( cbreed1DT );
@@ -61,10 +106,9 @@ axios.get("https://api.thecatapi.com/v1/images/search?breed_id=beng") //type of 
      cbreed1DL.appendChild( cNameDT );
      cbreed1DL.appendChild( cNameDD );
     // Put this into the page (<body>)!
+    document.body.appendChild( cHeading );
     document.body.appendChild( cbreed1DL );
-
 
  });
 
-
-
+ 
